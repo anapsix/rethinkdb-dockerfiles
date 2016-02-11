9802efd2cbd79276259e7ff9364f4fb60cd3b33c
Shawn McElroy <shawn@skift.io>
2016-02-11 08:02:32 -0800

    changing from  to

--- ubuntu/trusty/1.13.4/docker-entrypoint.sh
+++ ubuntu/trusty/1.13.4/docker-entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
-  [ -n "$TUTUM_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $TUTUM_CONTAINER_FQDN )
-  [ -n "$TUTUM_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
-  if [ -n "$TUTUM_SERVICE_FQDN" ]; then
-    for host in $(host -s -t a $TUTUM_SERVICE_FQDN | awk '{print $NF}'); do
+  [ -n "$DOCKERCLOUD_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $DOCKERCLOUD_CONTAINER_FQDN )
+  [ -n "$DOCKERCLOUD_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
+  if [ -n "$DOCKERCLOUD_SERVICE_FQDN" ]; then
+    for host in $(host -s -t a $DOCKERCLOUD_SERVICE_FQDN | awk '{print $NF}'); do
       TUTUM_ARGS+=( --join $host )
     done
   fi
-  [ -n "$TUTUM_ARGS" ] && TUTUM_ARGS+=( --bind all )
+  [ -n "$DOCKERCLOUD_ARGS" ] && TUTUM_ARGS+=( --bind all )
   set -- rethinkdb "$@" "${TUTUM_ARGS[@]}"
 fi
 
