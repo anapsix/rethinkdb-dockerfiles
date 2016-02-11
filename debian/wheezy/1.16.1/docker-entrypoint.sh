4e1c3925c2ca68909e00471d4a3b3a97b265897e
Shawn McElroy <shawn@skift.io>
2016-02-11 08:45:58 -0800

    found more instances of TUTUM, changed to DOCKERCLOUD

--- debian/wheezy/1.16.1/docker-entrypoint.sh
+++ debian/wheezy/1.16.1/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 set -e
 
 if [ ${#} -eq 0 ] || [ "${1:0:1}" = '-' ]; then
-  [ -n "$DOCKERCLOUD_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $DOCKERCLOUD_CONTAINER_FQDN )
-  [ -n "$DOCKERCLOUD_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
+  [ -n "$DOCKERCLOUD_CONTAINER_FQDN" ] && DOCKERCLOUD_ARGS+=( --canonical-address $DOCKERCLOUD_CONTAINER_FQDN )
+  [ -n "$DOCKERCLOUD_IP_ADDRESS" ] && DOCKERCLOUD_ARGS+=( --canonical-address ${DOCKERCLOUD_IP_ADDRESS%/*} )
   if [ -n "$DOCKERCLOUD_SERVICE_HOSTNAME" ]; then
     TRY_UNTIL=$(date +%s -d "+ 30 seconds")
-    TUTUM_CLUSTER_HOSTS=0
+    DOCKERCLOUD_CLUSTER_HOSTS=0
     for host in $(host -s -t a $DOCKERCLOUD_SERVICE_HOSTNAME | awk '{print $NF}'); do
       echo -n "waiting for $host to become available: " >&2
       while test $[$TRY_UNTIL-$(date +%s)] -gt 0; do
         if ping -q -w 1 -c 1 $host >/dev/null 2>&1; then
           echo "ok" >&2
-          let TUTUM_CLUSTER_HOSTS+=1
-          TUTUM_ARGS+=( --join $host )
+          let DOCKERCLOUD_CLUSTER_HOSTS+=1
+          DOCKERCLOUD_ARGS+=( --join $host )
           break
         else
           echo -n "." >&2
@@ -25,8 +25,8 @@ if [ ${#} -eq 0 ] || [ "${1:0:1}" = '-' ]; then
       echo "only one node started or cluster discovery timed-out" >&2
     fi
   fi
-  #[ -n "$DOCKERCLOUD_ARGS" ] && TUTUM_ARGS+=( --bind all )
-  set -- rethinkdb "$@" "${TUTUM_ARGS[@]}"
+  #[ -n "$DOCKERCLOUD_ARGS" ] && DOCKERCLOUD_ARGS+=( --bind all )
+  set -- rethinkdb "$@" "${DOCKERCLOUD_ARGS[@]}"
 fi
 
 exec "$@"
--- ubuntu/trusty/1.13.0/docker-entrypoint.sh
+++ ubuntu/trusty/1.13.0/docker-entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
-  [ -n "$DOCKERCLOUD_CONTAINER_FQDN" ] && TUTUM_ARGS+=( --canonical-address $DOCKERCLOUD_CONTAINER_FQDN )
-  [ -n "$DOCKERCLOUD_IP_ADDRESS" ] && TUTUM_ARGS+=( --canonical-address ${TUTUM_IP_ADDRESS%/*} )
+  [ -n "$DOCKERCLOUD_CONTAINER_FQDN" ] && DOCKERCLOUD_ARGS+=( --canonical-address $DOCKERCLOUD_CONTAINER_FQDN )
+  [ -n "$DOCKERCLOUD_IP_ADDRESS" ] && DOCKERCLOUD_ARGS+=( --canonical-address ${DOCKERCLOUD_IP_ADDRESS%/*} )
   if [ -n "$DOCKERCLOUD_SERVICE_FQDN" ]; then
     for host in $(host -s -t a $DOCKERCLOUD_SERVICE_FQDN | awk '{print $NF}'); do
-      TUTUM_ARGS+=( --join $host )
+      DOCKERCLOUD_ARGS+=( --join $host )
     done
   fi
-  [ -n "$DOCKERCLOUD_ARGS" ] && TUTUM_ARGS+=( --bind all )
-  set -- rethinkdb "$@" "${TUTUM_ARGS[@]}"
+  [ -n "$DOCKERCLOUD_ARGS" ] && DOCKERCLOUD_ARGS+=( --bind all )
+  set -- rethinkdb "$@" "${DOCKERCLOUD_ARGS[@]}"
 fi
 
 exec "$@"
